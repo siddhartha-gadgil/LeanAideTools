c18e62bcb22791a9e9adad8af25842d9a2263669
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2025-01-29 08:40:22 +0000

    wrappers for calling LeanAide.

---
+++ LeanAideToolsTest/Client.lean
@@ -0,0 +1,45 @@
+import Lean
+open Lean
+
+namespace LeanAideTools
+
+register_option leanaide.url : String := {
+  defValue := "http://localhost:7654"
+  descr := "URL of the LeanAide server"
+  group := "leanaide"
+}
+
+def leanaideUrl : CoreM String := do
+  return leanaide.url.get (← getOptions)
+
+def callLeanAide (data: Json) (url: String := "http://localhost:7654") : IO <| Json := do
+  let out ←
+    IO.Process.output
+      {cmd:= "curl", args:=#["-X", "POST", "-H", "\"Content-Type: application/json\"", "-d", data.compress, url]}
+  if out.exitCode != 0 then IO.throwServerError out.stderr
+  match Json.parse out.stdout with
+  | Except.error e =>
+    IO.throwServerError s!"Error parsing server output as Json:{e}; output: {out.stdout}"
+  | Except.ok js => return js
+
+def translateTheorem (s: String) (url: String := "http://localhost:7654") : IO Json := do
+  let js := Json.mkObj [("task", "translate_thm"), ("text", s)]
+  callLeanAide js url
+
+def translateDef (s: String) (url: String := "http://localhost:7654") : IO Json := do
+  let js := Json.mkObj [("task", "translate_def"), ("text", s)]
+  callLeanAide js url
+
+def theoremDoc (cmd name: String) (url: String := "http://localhost:7654") : IO Json := do
+  let js := Json.mkObj [("task", "theorem_doc"), ("name", name), ("command", cmd)]
+  callLeanAide js url
+
+def defDoc (cmd name: String) (url: String := "http://localhost:7654") : IO Json := do
+  let js := Json.mkObj [("task", "def_doc"), ("name", name), ("command", cmd)]
+  callLeanAide js url
+
+-- #eval translateTheorem "There are infinitely many prime numbers."
+
+-- #eval defDoc "def isCube (n: Nat) : Prop := ∃ m, m * m * m = n" "isCube"
+
+end LeanAideTools
